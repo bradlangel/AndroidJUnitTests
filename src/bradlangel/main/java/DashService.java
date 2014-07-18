08e585f37e43328c8e7046b6beb35fea14433c8a
bradlangel <b.a.langel@gmail.com>
2014-07-18 12:42:42 -0500

    added callback

--- src/bradlangel/main/java/DashService.java
+++ src/bradlangel/main/java/DashService.java
@@ -1,7 +1,22 @@
 package bradlangel.main.java;
 
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+import java.util.List;
 /**
  * Created by bradlangel on 7/18/14.
  */
 public interface DashService {
+
+
+
+    @GET("/gas/price/latlng/{latitude}/{longitude}/{sort}")
+    void getGasStations(@Path("latitude") String latitude,
+                                     @Path("longitude") String longitude,
+                                     @Path("sort") String sort,
+                                     Callback<List<GasStation>> callback);
 }
+
