d459fc9c811250f3486d08cf95f140b3f232c33a
bradlangel <b.a.langel@gmail.com>
2014-07-18 12:44:21 -0500

    correct test

--- src/bradlangel/main/test/DashApiTest.java
+++ src/bradlangel/main/test/DashApiTest.java
@@ -1,8 +1,15 @@
 package bradlangel.main.test;
 
-import bradlangel.GasStation;
+import bradlangel.main.java.ApiCredentials;
+import bradlangel.main.java.DashService;
+import bradlangel.main.java.GasStation;
+import org.junit.Assert;
 import org.junit.Test;
+import retrofit.Callback;
+import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 import java.util.List;
 
@@ -14,24 +21,53 @@ public class DashApiTest {
     private final static String latitude = "40.686647";
     private final static String longitude = "-73.991809";
     private final static String sort = "regular";
-
+    private final ApiCredentials apiCredentials = new ApiCredentials();
 
 
     @Test
     public void callToAPI() {
-//Create Rest Adaptor; explicitly call Okhttp
+
+
+
+
+
+        RequestInterceptor requestInterceptor = new RequestInterceptor() {
+            @Override
+            public void intercept(RequestFacade request) {
+                request.addHeader(apiCredentials.getKey(), apiCredentials.getValue());
+            }
+        };
+
 
         RestAdapter restAdapterT =  new RestAdapter.Builder()
                 .setEndpoint("https://dash.by/api/v1")
+                .setRequestInterceptor(requestInterceptor)
                 .build();
 
-        DashApiInterface dashApiServiceT = restAdapterT.create(DashApiInterface.class);
+        DashService dashApiServiceT = restAdapterT.create(DashService.class);
+
+
 
 
         //Each call on the generated dashApiService makes an HTTP request to the remote web server.
-        List<GasStation> gasStationsT = dashApiServiceT.listGasStations(latitude, longitude, sort, key, value);
+        dashApiServiceT.getGasStations(latitude,
+                longitude,
+                sort,
+                new Callback<List<GasStation>>() {
+                    @Override
+                    public void success(List<GasStation> gasStations, Response response) {
+                        String gasLocationT = gasStations.get(0).getLongName();
+                        System.out.print(gasLocationT);
+                        Assert.assertTrue(gasLocationT.equals("Brooklyn Hess"));
+                    }
 
-        String gasLocationT = gasStationsT.get(0).getLongName();
+                    @Override
+                    public void failure(RetrofitError retrofitError) {
+
+                    }
+                });
+
+
 
     }
 }
